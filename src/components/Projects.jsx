a00adb795d10d743cc62cbf72074540d51abae9c
Dali <dalihelal6@gmail.com>
2022-10-04 18:14:05 +0100

    add second project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import "../styles/projects.scss"
 import image1 from "../images/project1.PNG"
-import { FaReact, FaHtml5, FaCss3Alt, FaJs, FaNodeJs,FaGithub,FaEye } from "react-icons/fa"
+import image2 from "../images/project2.PNG"
+import { FaReact, FaHtml5, FaCss3Alt, FaJs, FaNodeJs,FaGithub,FaEye,FaSass } from "react-icons/fa"
 import {SiRedux } from "react-icons/si"
 const Projects = () => {
     return (
@@ -36,6 +37,40 @@ const Projects = () => {
                         <a href="https://github.com/dali-helal/watch-store"><FaGithub className="links-icons" color="#777777" /></a>
                         
                         <a href="https://watch-store-eight.vercel.app"><FaEye className="links-icons" color="#777777"/></a>
+                        
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={image2} alt="" />
+                    <h3>Yammi Food </h3>
+                    <p>In this project I tried to clone the plateforme YASSER EXPRESS (delivery food) . I used the these technologies :</p>
+                    <div className="technologie">
+                        <div>
+                            <FaHtml5 className="icons" color="#F0652A" />
+                        </div>
+                        <div>
+                            <FaCss3Alt className="icons" color="#1C88C7" />
+                        </div>
+                        <div>
+                            <FaJs className="icons" color="#F0DB4F" />
+                        </div>
+                        <div>
+                            <FaReact className="icons" color="#00CCFF" />
+                        </div>
+                        <div>
+                            <SiRedux className="icons" color="#764ABC" />
+                        </div>
+                        <div>
+                            <FaNodeJs className="icons" color="#4EB045" />
+                        </div>
+                        <div>
+                            <FaSass className="icons" color="#CF649A" />
+                        </div>
+                    </div>
+                    <div className="links">
+                        <a href="https://github.com/dali-helal/delivery-project"><FaGithub className="links-icons" color="#777777" /></a>
+                        
+                        <a href="https://delivery-project-three.vercel.app"><FaEye className="links-icons" color="#777777"/></a>
                         
                     </div>
                 </div>
